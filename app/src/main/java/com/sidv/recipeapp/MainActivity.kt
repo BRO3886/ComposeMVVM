411059680f35f05380fb93a18355d4f809506e36
Siddhartha Varma <siddverma1999@gmail.com>
2021-04-10 15:33:47 +0530

    textstyles and images

--- app/src/main/java/com/sidv/recipeapp/MainActivity.kt
+++ app/src/main/java/com/sidv/recipeapp/MainActivity.kt
@@ -2,14 +2,20 @@ package com.sidv.recipeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.imageFromResource
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 class MainActivity : AppCompatActivity() {
@@ -20,10 +26,35 @@ class MainActivity : AppCompatActivity() {
             Column(
                 modifier = Modifier.padding(16.dp)
             ) {
-                Text(text = "This is some text")
-                Spacer(modifier = Modifier.padding(top = 20.dp))
-                Button(onClick = { /*TODO*/ }) {
-                    Text(text = "SUBMIT")
+                Image(
+                    bitmap = imageFromResource(
+                        res = resources,
+                        resId = R.drawable.howcuttingdo
+                    ),
+                    contentDescription = "image",
+                )
+                Column(
+                    modifier = Modifier.padding(16.dp)
+                )
+                {
+                    Text(
+                        text = "This is some text",
+                        style = TextStyle(
+                            fontSize = 26.sp,
+                        )
+                    )
+                    Spacer(modifier = Modifier.padding(top = 20.dp))
+                    Text(
+                        text = "This is some text",
+                        style = TextStyle(
+                            color = Color(0xFF85bb65)
+                        )
+                    )
+                    Spacer(modifier = Modifier.padding(top = 10.dp))
+                    Text(text = "This is some text")
+                    Button(onClick = { /*TODO*/ }) {
+                        Text(text = "SUBMIT")
+                    }
                 }
             }
         }
