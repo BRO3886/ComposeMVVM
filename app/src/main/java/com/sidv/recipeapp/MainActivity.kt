ad8aea69027194f0507c7c0642f668e8888dab57
Siddhartha Varma <siddverma1999@gmail.com>
2021-04-10 15:19:36 +0530

    set content is deprecated but I still use it

--- app/src/main/java/com/sidv/recipeapp/MainActivity.kt
+++ app/src/main/java/com/sidv/recipeapp/MainActivity.kt
@@ -2,14 +2,31 @@ package com.sidv.recipeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.unit.dp
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text(text = "This is some text")
+                Spacer(modifier = Modifier.padding(top = 20.dp))
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "SUBMIT")
+                }
+            }
         }
     }
+
 }
