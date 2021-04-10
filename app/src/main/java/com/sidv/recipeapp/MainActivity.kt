6cb67b04c4ff9f119a5428c8428c85e733915cbc
Siddhartha Varma <siddverma1999@gmail.com>
2021-04-10 14:42:28 +0530

    Initial commit

---
+++ app/src/main/java/com/sidv/recipeapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.sidv.recipeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.compose.ui.platform.setContent
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+
+        }
+    }
+}
