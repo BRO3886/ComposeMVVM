5c58caf1b874d3704da27a320de332e40108d129
Siddhartha Varma <siddverma1999@gmail.com>
2021-04-11 13:13:26 +0530

    composable within a fragment woeeeh

--- app/src/main/java/com/sidv/recipeapp/RecipeListFragment.kt
+++ app/src/main/java/com/sidv/recipeapp/RecipeListFragment.kt
@@ -4,8 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.fragment.app.Fragment
 
 //or Fragment(R.layout.fragment_recipe_list)
@@ -14,12 +25,22 @@ class RecipeListFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        return ComposeView(requireContext()).apply {
-            setContent {
-                Text(text = "Composable fragment")
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_recipe_list, container, false)
+        view.findViewById<ComposeView>(R.id.compose_view).setContent {
+            Column(modifier = Modifier
+                .border(border = BorderStroke(1.dp, Color.Black))
+                .padding(16.dp)
+            ) {
+                Text(text = "This is a composable inside a fragment")
+                Spacer(modifier = Modifier.padding(10.dp))
+                CircularProgressIndicator()
+                Text(text = "interop good, below this is a standard kt fragment")
+                Spacer(modifier = Modifier.padding(10.dp))
+                val customView = HorizontalDottedProgress(LocalContext.current)
+                AndroidView(viewBlock = {customView})
             }
         }
-
+        return  view
     }
 }
