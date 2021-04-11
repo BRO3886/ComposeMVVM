0b5ca05db5f6a12041a0bc49a6acd2cbd6f97f09
Siddhartha Varma <siddverma1999@gmail.com>
2021-04-11 12:29:42 +0530

    composable fragment, layout not needed

--- app/src/main/java/com/sidv/recipeapp/RecipeListFragment.kt
+++ app/src/main/java/com/sidv/recipeapp/RecipeListFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.material.Text
+import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 
 //or Fragment(R.layout.fragment_recipe_list)
@@ -12,8 +14,14 @@ class RecipeListFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_recipe_list,container, false)
+    ): View {
+        val view = ComposeView(requireContext())
+        view.apply {
+            setContent {
+                Text(text = "Composable fragment")
+            }
+        }
+
         return view
     }
 }
